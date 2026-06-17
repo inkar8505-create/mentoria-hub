e10659e8186ca1e4ac7c6fc46abdefb14dbdc44a
inkar8505-create <inkar8505@gmail.com>
2026-06-17 23:56:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,41 @@ function show(path) {
     data[path].map(i => "<li>" + i + "</li>").join("") +
     "</ul>";
 }
+function saveProfile() {
+  let user = {
+    name: document.getElementById("name").value,
+    age: document.getElementById("age").value,
+    interest: document.getElementById("interest").value,
+    goal: document.getElementById("goal").value
+  };
+
+  localStorage.setItem("user", JSON.stringify(user));
+
+  showDashboard(user);
+}
+
+function showDashboard(user) {
+  document.body.innerHTML = `
+    <h1>Hello ${user.name}</h1>
+    <h2>Your path: ${user.interest}</h2>
+
+    <button onclick="showOpportunities('${user.interest}')">
+      See Opportunities
+    </button>
+
+    <div id="result"></div>
+  `;
+}
+
+function showOpportunities(path) {
+  let data = {
+    English: ["IELTS", "FLEX", "Essay contests"],
+    STEM: ["Olympiads", "Hackathons", "NASA programs"],
+    Business: ["Startups", "Case competitions", "Internships"]
+  };
+
+  document.getElementById("result").innerHTML =
+    "<ul>" +
+    data[path].map(x => "<li>" + x + "</li>").join("") +
+    "</ul>";
+}
